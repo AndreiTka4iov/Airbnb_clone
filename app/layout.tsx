14069c605f21a3ed6825d6115b2823177df9fb28
Andrei Tkachev <andrei.tkachiov@gmail.com>
2023-04-26 11:57:26 +0300

    modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/navbar/Navbar'
 import ClientOnly from './components/ClientOnly'
-import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
+import ToasterProvider from './providers/ToasterProvider'
 
 const font = Nunito({ 
   subsets: ['latin'],
@@ -23,7 +24,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal />
+          <ToasterProvider />
+          <RegisterModal />
           <Navbar/>
           {children}
         </ClientOnly>
