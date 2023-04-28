812194dea45858b56d353d30ce8aaa0222bd3649
Andrei Tkachev <andrei.tkachiov@gmail.com>
2023-04-28 13:07:37 +0300

    favorite

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,9 @@ export default async function RootLayout({
           <LoginModal />
           <RegisterModal />
           <Navbar currentUser={currentUser}/>
-          {children}
+          <div className='pb-20 pt-28'>
+            {children}
+          </div>
         </ClientOnly>
       </body>
     </html>
